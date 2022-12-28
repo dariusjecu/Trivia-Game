d5eb214b143c22d6e42f4450cee3cb24012d51fd
dariusjecu <dariusjecu@yahoo.com>
2022-12-28 17:12:42 +0000

    Responsive added

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import First from "../public/paint1.png"
+import Second from "../public/paint2.png"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <div>
         <div className='images'>
-            <img className='first-img' src="../public/paint1.png" />
-            <img className='second-img' src="../public/paint2.png" />
+            <img className='first-img' src={First} />
+            <img className='second-img' src={Second} />
         </div>
         <App />
     </div>
